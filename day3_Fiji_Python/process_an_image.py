7448c5b2d14ba9ca4ec035c42d360ae2989d5110
haesleinhuepf <rhaase@mpi-cbg.de>
2018-04-06 13:50:41 +0200

    process a whole folder

--- day3_Fiji_Python/process_an_image.py
+++ day3_Fiji_Python/process_an_image.py
@@ -1,13 +1,33 @@
 from ij import IJ
 from ij import Prefs
+from java.io import File
+from ij.measure import ResultsTable
 
-# load image and show it
-imp = IJ.openImage("C:/structure/code/IMPRS-programming-course/day3_Fiji_Python/data/blobs.tif");
-imp.show();
+# configuration
+folder = "C:/structure/code/IMPRS-programming-course/day3_Fiji_Python/data/";
 
-# apply a threshold to detect objects (cells, nuclei,...)
-IJ.setAutoThreshold(imp, "Otsu");
-Prefs.blackBackground = False;
-IJ.run(imp, "Convert to Mask", "");
+# access to the folder and the files in it
+directory = File(folder);
+listOfFilesInFolder = directory.listFiles();
+
+for file in listOfFilesInFolder:
+
+	# load image and show it
+	imp = IJ.openImage(file.toString());
+	imp.show();
+	
+	# apply a threshold to detect objects (cells, nuclei,...)
+	IJ.setAutoThreshold(imp, "Otsu");
+	Prefs.blackBackground = False;
+	IJ.run(imp, "Convert to Mask", "");
+	
+	# analyse objects and put results in table
+	IJ.run("Set Measurements...", "area display redirect=None decimal=5");
+	IJ.run(imp, "Analyze Particles...", "display exclude");
+	
+
+
+
+
 
 
