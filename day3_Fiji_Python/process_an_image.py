aa5986e32ce081468ff21dd2c3961209a1931fe7
haesleinhuepf <rhaase@mpi-cbg.de>
2018-04-06 14:12:13 +0200

    save results in a table for each file inidivdually

--- day3_Fiji_Python/process_an_image.py
+++ day3_Fiji_Python/process_an_image.py
@@ -4,30 +4,34 @@ from java.io import File
 from ij.measure import ResultsTable
 
 # configuration
-folder = "C:/structure/code/IMPRS-programming-course/day3_Fiji_Python/data/";
+folder = "C:/structure/code/IMPRS-programming-course/day3_Fiji_Python/data";
 
 # access to the folder and the files in it
 directory = File(folder);
 listOfFilesInFolder = directory.listFiles();
 
 for file in listOfFilesInFolder:
+	if (file.toString().endswith(".tif")):
+		# load image and show it
+		imp = IJ.openImage(file.toString());
+		imp.show();
+	
+		# apply a threshold to detect objects (cells, nuclei,...)
+		IJ.setAutoThreshold(imp, "Otsu");
+		Prefs.blackBackground = False;
+		IJ.run(imp, "Convert to Mask", "");
+		
+		# analyse objects and put results in table
+		IJ.run("Set Measurements...", "area display redirect=None decimal=5");
+		IJ.run(imp, "Analyze Particles...", "display exclude");
+		
+		# save the table
+		table = ResultsTable.getResultsTable();
+		table.show("Results");
+		table.save(file.toString() + "results.xls");
+		table.reset()
+		
 
-	# load image and show it
-	imp = IJ.openImage(file.toString());
-	imp.show();
-	
-	# apply a threshold to detect objects (cells, nuclei,...)
-	IJ.setAutoThreshold(imp, "Otsu");
-	Prefs.blackBackground = False;
-	IJ.run(imp, "Convert to Mask", "");
-	
-	# analyse objects and put results in table
-	IJ.run("Set Measurements...", "area display redirect=None decimal=5");
-	IJ.run(imp, "Analyze Particles...", "display exclude");
-	
-	table = ResultsTable.getResultsTable();
-	table.save(folder + "results.xls");
-	
 	
 
 
